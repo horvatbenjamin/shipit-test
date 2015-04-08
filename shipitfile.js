44326da4fab726be3f780690132913665d41c9a8
Benjamin Horvat <benjamin@benjamin-laptop>
2015-04-08 15:04:11 +0200

    proba

---
+++ shipitfile.js
@@ -0,0 +1,18 @@
+odule.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/github-monitor',
+      deployTo: '/tmp/deploy_to',
+      repositoryUrl: 'https://github.com/horvatbenjamin/shipit-test.git',
+      ignores: ['.git', 'node_modules'],
+      rsync: ['--del'],
+      keepReleases: 2,
+      shallowClone: true
+    },
+    staging: {
+      servers: 'deploy@mad.itk.ppke.hu'
+    }
+  });
+};
